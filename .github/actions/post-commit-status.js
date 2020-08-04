244898a2cb6aaa1d0fa3b65ed6c5efe834ac1c0f
devmachiine <46693400+devmachiine@users.noreply.github.com>
2020-08-04 21:08:23 +0000

    js asi failture?

--- .github/actions/post-commit-status.js
+++ .github/actions/post-commit-status.js
@@ -3,20 +3,20 @@ const { Octokit } = require("@octokit/action");
 const octokit = new Octokit();
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
-const sha = process.env.GITHUB_SHA
+const sha = process.env.GITHUB_SHA;
 
-    (async () => {
-        // See https://docs.github.com/en/rest/reference/repos#create-a-commit-status
-        const { data } = await octokit.request("POST /repos/:owner/:repo/statuses/:sha", {
-            owner,
-            repo,
-            sha,
-            state: "success",
-            description: "all good",
-            context: "github action: post-commit-status.js"
-        });
+(async () => {
+    // See https://docs.github.com/en/rest/reference/repos#create-a-commit-status
+    const { data } = await octokit.request("POST /repos/:owner/:repo/statuses/:sha", {
+        owner,
+        repo,
+        sha,
+        state: "success",
+        description: "all good",
+        context: "github action: post-commit-status.js"
+    });
 
-        console.log(`Commit ${sha} updated: ${data.html_url}`)
-    })()
+    console.log(`Commit ${sha} updated: ${data.html_url}`)
+})();
 
 
