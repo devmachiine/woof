f0ade151dd1ff372a9ff9318aefd19f6a6dfef4b
devmachiine <46693400+devmachiine@users.noreply.github.com>
2020-08-04 20:41:01 +0000

    wrap await in iiafe

--- .github/actions/post-commit-status.js
+++ .github/actions/post-commit-status.js
@@ -3,15 +3,19 @@ const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
 const sha = process.env.GITHUB_SHA
 
-// See https://docs.github.com/en/rest/reference/repos#create-a-commit-status
-//                                           /repos/{owner}/{repo}/statuses/{sha}
-const { data } = await octokit.request("POST /repos/:owner/:repo/statuses/:sha", {
-  owner,
-  repo,
-  sha,
-  state: "success",
-  description: "all good",
-  context: "github action: post-commit-status.js"
-});
+    (async () => {
+        // See https://docs.github.com/en/rest/reference/repos#create-a-commit-status
+        //                                           /repos/{owner}/{repo}/statuses/{sha}
+        const { data } = await octokit.request("POST /repos/:owner/:repo/statuses/:sha", {
+            owner,
+            repo,
+            sha,
+            state: "success",
+            description: "all good",
+            context: "github action: post-commit-status.js"
+        });
 
-console.log(`Commit ${sha} updated: ${data.html_url}`)
+        console.log(`Commit ${sha} updated: ${data.html_url}`)
+    })()
+
+
